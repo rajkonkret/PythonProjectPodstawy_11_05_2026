14fac25fce429b5f1223efe7b4d35576083a50f5
rajkonkret <rajkonkret660@gmail.com>
2026-05-11 15:41:38 +0200

    [1 31] update  typy_danych_1.py.py

--- day_1/typy_danych_1.py
+++ day_1/typy_danych_1.py
@@ -1,3 +1,5 @@
+import sys
+
 wiek = 47  # int
 rok = 2026  # int
 temp = 36.6  # float
@@ -31,4 +33,17 @@ print(len(str(wiek ** rok)))  # 3388
 print(54 - 5 * 43 + 4 / 2 + 4 / 2)  # -157.0
 print(54 - 5 * 43 + (4 / 2 + 4) / 2)  # -158.0
 
+print(0.2 + 0.8)  # 1.0
+print(0.2 + 0.7)  # 0.8999999999999999 == 0.9 fałsz
+print(0.1 + 0.3)  # 0.4
+print(0.1 + 0.2)  # 0.30000000000000004
 
+# For example, in a floating-point arithmetic with five base-ten digits,
+# the sum 12.345 + 1.0001 = 13.3451 might be rounded to 13.345
+
+print(sys.float_info)
+# sys.float_info(max=1.7976931348623157e+308, max_exp=1024, max_10_exp=308,
+# min=2.2250738585072014e-308, min_exp=-1021, min_10_exp=-307, dig=15,
+# mant_dig=53, epsilon=2.220446049250313e-16, radix=2, rounds=1)
+
+# decimal() - pozwala ominąć problem zaokrąglenia
