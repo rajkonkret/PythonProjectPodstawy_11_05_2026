40b3853677f0cac48e380c6a19e8cbe2fe8c3085
rajkonkret <rajkonkret660@gmail.com>
2026-05-11 11:52:42 +0200

    [1 4] add pierwszy.py

---
+++ day_1/pierwszy.py
@@ -0,0 +1,7 @@
+print("Hello World")
+print("Witaj Świecie")
+# ctrl alt l - formatowanie pep8
+# cd .\day_1\ - przejscie do katologu day_1
+# (.venv) PS C:\Users\CSComarch\PycharmProjects\PythonProjectPodstawy_11_05_2026\day_1> python .\pierwszy.py
+# Hello World
+# Witaj Świecie
