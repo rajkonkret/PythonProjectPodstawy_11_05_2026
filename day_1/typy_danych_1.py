4e6a2771af235d08785d9feccfeaab62ee10b501
rajkonkret <rajkonkret660@gmail.com>
2026-05-11 15:27:15 +0200

    [1 29] update  formaty.py

---
+++ day_1/typy_danych_1.py
@@ -0,0 +1,29 @@
+wiek = 47  # int
+rok = 2026  # int
+temp = 36.6  # float
+
+print(wiek + rok)  # 2073
+print(wiek - rok)  # -1979
+print(wiek * rok)  # 95222
+print(wiek / rok)  # 0.02319842053307009 -> float
+
+print(2026 / 47)  # 43.1063829787234
+print(2026 // 47)  # 43, część calkowita
+
+print(rok % wiek)  # modulo, reszta z dzielenia
+print(2026 % 47)
+print(43 * 47)  # 2021
+print(2026 - 2021)  # 5
+
+print(10 % 3)  # reszta 1
+# ctrl shift f - wyszukiwanie w projekcie
+
+print(wiek ** rok)  # potęgowanie
+
+# len() - długość
+# str() - rzutowanie/zamiana na tekst
+print(len(str(wiek ** rok)))  # 3388
+
+# print(len(str(wiek ** rok ** 2)))  # 3388
+# ValueError: Exceeds the limit (4300 digits) for integer string conversion;
+# use sys.set_int_max_str_digits() to increase the limit
