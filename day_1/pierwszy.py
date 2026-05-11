42b2a5442077c2c8dbe775d76af3aa249b9f6cf0
rajkonkret <rajkonkret660@gmail.com>
2026-05-11 11:57:13 +0200

    [1 5] add pierwszy.py

--- day_1/pierwszy.py
+++ day_1/pierwszy.py
@@ -5,3 +5,14 @@ print("Witaj Świecie")
 # (.venv) PS C:\Users\CSComarch\PycharmProjects\PythonProjectPodstawy_11_05_2026\day_1> python .\pierwszy.py
 # Hello World
 # Witaj Świecie
+
+# ctrl / - komentarz
+# print('Hello")
+#   File "C:\Users\CSComarch\PycharmProjects\PythonProjectPodstawy_11_05_2026\day_1\pierwszy.py", line 9
+#     print('Hello")
+#           ^
+# SyntaxError: unterminated string literal (detected at line 9)
+#
+# Process finished with exit code 1 - mamy błąd
+print("Dalsza część")
+# Process finished with exit code 0 - program dziła poprawnie
