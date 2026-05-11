93f644088fec92881fd1f681a2b651aa1e368be0
rajkonkret <rajkonkret660@gmail.com>
2026-05-11 14:34:54 +0200

    [1 24] update  formaty.py

--- day_1/formaty.py
+++ day_1/formaty.py
@@ -9,4 +9,13 @@ print("Witaj %s, masz teraz %d lat." % (user, wiek))
 # Witaj Tomek, masz teraz 39 lat.
 # %s - string
 # %d - digit
-print("Witaj %d, masz teraz %s lat." % (user, wiek))
+
+# print("Witaj %d, masz teraz %s lat." % (user, wiek))
+# TypeError: %d format: a real number is required, not str
+
+# f-string -> Witaj Tomek, masz teraz 39 lat.
+print(f'Witaj {user}, masz teraz {wiek} lat.')
+# Witaj Tomek, masz teraz 39 lat.
+
+# %i - liczba całkowita (integer)
+# %f: formatowanie liczb zmiennoprzecinkowych
