bb93812e26d8a425f4cdf7f44479f097b3573097
rajkonkret <rajkonkret660@gmail.com>
2026-05-12 12:52:06 +0200

    [2 33] update  typy_danych_5_slownik.py

--- day_2/typy_danych_5_slownik.py
+++ day_2/typy_danych_5_slownik.py
@@ -20,3 +20,34 @@ print(dictionary)  # {'imie': 'Radek'}
 # dodac klucz 'wiek'
 dictionary["wiek"] = 50
 print(dictionary)  # {'imie': 'Radek', 'wiek': 50}
+
+print(dictionary.keys())
+print(dictionary.values())
+print(dictionary.items())
+# dict_keys(['imie', 'wiek'])
+# dict_values(['Radek', 50])
+# dict_items([('imie', 'Radek'), ('wiek', 50)])
+
+# nadpisanie wartości
+dictionary['imie'] = "Tomek"
+print(dictionary)  # {'imie': 'Tomek', 'wiek': 50}
+
+# wypisanie wartości dla klucza
+print(dictionary['imie'])  # Tomek
+
+dictionary['imie'] = ['Radek', "Tomek", "Magda"]
+print(dictionary)  # {'imie': ['Radek', 'Tomek', 'Magda'], 'wiek': 50}
+
+# wypisac Tomka
+print(dictionary['imie'][1])  # Tomek
+
+print(dictionary['imie'][1].lower())  # tomek
+print(dictionary['imie'][::-1])  # ['Magda', 'Tomek', 'Radek']
+
+dictionary_radek = {'imie': ['Radek', 'Tomek', 'Magda'], 'wiek': 50}
+print(dictionary_radek)  # {'imie': ['Radek', 'Tomek', 'Magda'], 'wiek': 50}
+
+# print(dictionary_radek['Imie'])  # KeyError: 'Imie'
+print(dictionary_radek['Imie'.lower()])  # ['Radek', 'Tomek', 'Magda']
+
+print(dictionary_radek.get("Imie"))
