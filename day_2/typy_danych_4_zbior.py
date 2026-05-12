41c86759d5b68329689f698159ecb7777d51f794
rajkonkret <rajkonkret660@gmail.com>
2026-05-12 12:08:30 +0200

    [2 25] update  typy_danych_3_krotka.py.py

---
+++ day_2/typy_danych_4_zbior.py
@@ -0,0 +1,15 @@
+# zbior (set) - przechowuje unikalne wartości
+# nie zachowuje kolejności przy dodawaniu elemntów
+# nie posiada indexu
+
+lista = [44, 55, 66, 777, 33, 22, 11, 33, 11, 777]
+zbior = set(lista)  # rzutowanie na zbiór
+print(type(zbior))  # <class 'set'>
+print(zbior)  # {33, 66, 777, 11, 44, 22, 55} zmiana kolejności
+
+lista_sort = sorted(zbior)  # dostaniemy liste, zbior nie ma indeksu
+print(lista_sort)  # [11, 22, 33, 44, 55, 66, 777]
+
+lista_sort = list(zbior)
+lista_sort.sort()
+print(lista_sort)  # [11, 22, 33, 44, 55, 66, 777]
