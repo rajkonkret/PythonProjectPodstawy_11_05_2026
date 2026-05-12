38a52e9ea621d926659da4533e25c3e5660f7cab
rajkonkret <rajkonkret660@gmail.com>
2026-05-12 11:38:52 +0200

    [2 21] update  typy_danych_3_krotka.py.py

--- day_2/typy_danych_3_krotka.py
+++ day_2/typy_danych_3_krotka.py
@@ -21,3 +21,18 @@ tupla_jeden = (45,)  # (45,)
 print(tupla_jeden)
 print(type(tupla_jeden))  # <class 'tuple'>
 
+# usunięcie calej tupli
+del tupla_jeden
+# print(tupla_jeden)  # NameError: name 'tupla_jeden' is not defined
+
+# tupla jest niemutowalna
+# tupla_liczby[0] = 123
+# TypeError: 'tuple' object does not support item assignment
+
+print(tupla_imiona)
+# ('Zenek', 'Marek', 'Radek', 'Ania')
+
+print(tupla_imiona.index("Radek"))  # index numer 2
+print(tupla_imiona.count("Radek"))  # występuje 1 raz
+
+print(len(tupla_imiona))  # długośc 4
