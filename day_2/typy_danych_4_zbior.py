f7c0260f382992b192b5a49dabfb844d432575d4
rajkonkret <rajkonkret660@gmail.com>
2026-05-12 12:31:38 +0200

    [2 30] update  typy_danych_4_zbior.py

--- day_2/typy_danych_4_zbior.py
+++ day_2/typy_danych_4_zbior.py
@@ -73,3 +73,16 @@ print(zbior_2.difference(zbior))  # {667, 52, 12.34, 62}
 zbior.update(zbior_2)
 print(zbior)
 # {777, 11, 44, 12.34, 18, 52, 22, 24, 25, 667, 62}
+
+krotka = tuple(zbior)
+print(krotka)  # (777, 11, 44, 12.34, 18, 52, 22, 24, 25, 667, 62)
+
+lista = list(zbior)
+print(lista)  # [777, 11, 44, 12.34, 18, 52, 22, 24, 25, 667, 62]
+
+# spradzenie czy dany element istnieje w kolekcji
+# in
+print(667 in zbior)  # True
+print(667 in lista)  # True
+print(667 in krotka)  # True
+print(767 in krotka)  # False
