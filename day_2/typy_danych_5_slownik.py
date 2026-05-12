86c2fd9a4c72261a997773ceef94127b87d003e0
rajkonkret <rajkonkret660@gmail.com>
2026-05-12 12:42:52 +0200

    [2 32] update  typy_danych_5_slownik.py

--- day_2/typy_danych_5_slownik.py
+++ day_2/typy_danych_5_slownik.py
@@ -12,3 +12,11 @@ print(type(dictionary))  # <class 'dict'>
 dictionary_1 = dict()
 print(dictionary_1)  # {}
 print(type(dictionary_1))  # <class 'dict'>
+
+# dodanie elementów do słownika
+dictionary['imie'] = 'Radek'
+print(dictionary)  # {'imie': 'Radek'}
+
+# dodac klucz 'wiek'
+dictionary["wiek"] = 50
+print(dictionary)  # {'imie': 'Radek', 'wiek': 50}
