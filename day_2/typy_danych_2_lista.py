f5a31c521253d0f04cce48f5766c06e1bcce5e9f
rajkonkret <rajkonkret660@gmail.com>
2026-05-12 10:03:46 +0200

    [2 7] update  typy_danych_2_lista.py

--- day_2/typy_danych_2_lista.py
+++ day_2/typy_danych_2_lista.py
@@ -63,3 +63,19 @@ print(lista[:])
 
 print(lista[-2:0])  # [4:0] -> []
 print(lista[0:-2])  # ['Radek', 'Tomek', 'Zenek', 'Anna']
+
+# 0 do 14
+# lisat_15 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+lista_15 = list(range(15))  # od 0 do 14
+print(lista_15)
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+print(lista_15[::2])  # [start:stop:krok] ->  [0, 2, 4, 6, 8, 10, 12, 14]
+print(lista_15[::3])  # [start:stop:krok] ->  [0, 3, 6, 9, 12]
+
+# wyswietlanie listę w odwrotnej kolejności
+print(lista_15[::-1])
+# [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+print(list(range(0, 15, 2)))  # (start, stop, krok)
+# [0, 2, 4, 6, 8, 10, 12, 14]
