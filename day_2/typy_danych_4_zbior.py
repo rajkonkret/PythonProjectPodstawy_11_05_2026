8948546baca27305ec72c58b963194221316c843
rajkonkret <rajkonkret660@gmail.com>
2026-05-12 12:15:53 +0200

    [2 26] update  typy_danych_4_zbior.py

--- day_2/typy_danych_4_zbior.py
+++ day_2/typy_danych_4_zbior.py
@@ -13,3 +13,27 @@ print(lista_sort)  # [11, 22, 33, 44, 55, 66, 777]
 lista_sort = list(zbior)
 lista_sort.sort()
 print(lista_sort)  # [11, 22, 33, 44, 55, 66, 777]
+
+# pusty zbior
+zb2 = set()  # tylko i wyłącznie słowkiem set()
+print(zb2)  # set() - pusty zbiór
+
+# dodanie elementu do zbioru
+zbior.add(33)
+zbior.add(33)
+zbior.add(33)
+zbior.add(33)
+zbior.add(33)
+zbior.add(33)
+zbior.add(33)
+zbior.add(18)
+zbior.add(33)
+zbior.add(33)
+zbior.add(18)
+zbior.add(24)
+zbior.add(24)
+zbior.add(33)
+zbior.add(25)
+
+print(zbior)
+# {33, 66, 777, 11, 44, 18, 22, 55, 24, 25}
