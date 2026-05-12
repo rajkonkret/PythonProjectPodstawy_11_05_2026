7601964c3f06f744b011d99340967aeaf53f96b5
rajkonkret <rajkonkret660@gmail.com>
2026-05-12 11:54:37 +0200

    [2 22] update  typy_danych_3_krotka.py.py

--- day_2/typy_danych_3_krotka.py
+++ day_2/typy_danych_3_krotka.py
@@ -56,5 +56,11 @@ print(a, b)  # 2 1
 print(tupla_imiona)  # ('Zenek', 'Marek', 'Radek', 'Ania')
 # name1, name2, name3
 # name1, name2, name3 = tupla_imiona # ValueError: too many values to unpack (expected 3, got 4)
-name1, name2, *name3 = tupla_imiona # * dowolna ilośc elementów
-print(name1, name2, name3) # Zenek Marek ['Radek', 'Ania']
+name1, name2, *name3 = tupla_imiona  # * dowolna ilośc elementów
+print(name1, name2, name3)  # Zenek Marek ['Radek', 'Ania']
+
+name1, *name2, name3 = tupla_imiona  # * dowolna ilośc elementów
+print(name1, name2, name3)  # Zenek ['Marek', 'Radek'] Ania
+
+*name1, name2, name3 = tupla_imiona  # * dowolna ilośc elementów
+print(name1, name2, name3)  # ['Zenek', 'Marek'] Radek Ania
