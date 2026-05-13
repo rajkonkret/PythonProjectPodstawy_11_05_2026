5b13bf70653451c7826ec6ad8a5924e1694a5ffb
rajkonkret <rajkonkret660@gmail.com>
2026-05-13 15:54:40 +0200

    [3 45] update  csv_zad1.py

--- day_3/csv_zad1.py
+++ day_3/csv_zad1.py
@@ -29,3 +29,25 @@ with open(filename, "w", newline="") as f:
     csvwriter = csv.DictWriter(f, fieldnames=fields)
     csvwriter.writeheader()  # zapis nagłówka, nazwy kolumn
     csvwriter.writerow(dict_name)
+
+products = [
+    {'sku': 1, "exp_date": 'today', "price": 200},
+    {'sku': 2, "exp_date": 'today', "price": 200},
+    {'sku': 3, "exp_date": 'tomorrow', "price": 200},
+    {'sku': 4, "exp_date": 'today', "price": 200},
+    {'sku': 5, "exp_date": 'tomorrow', "price": 200},
+    {'sku': 6, "exp_date": 'today', "price": 200},
+]
+
+filename = 'records_discount.csv'
+
+# list_product = ['sku', 'exp_date', 'price']
+
+# pobieramy nazwy kluczy (kolumn)
+list_products = [key for key in products[0]]
+
+with open(filename, "w", newline="") as f:
+    # csvwriter = csv.DictWriter(f, fieldnames=list_products, delimiter=",")
+    csvwriter = csv.DictWriter(f, fieldnames=list_products, delimiter=";")
+    csvwriter.writeheader()  # nagłówki
+    csvwriter.writerows(products)  # writerows - podajemy listę słowników
