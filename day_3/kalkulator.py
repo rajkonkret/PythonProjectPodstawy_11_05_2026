b6d47bcac0423d02f6d8a93a8aabed590c505e60
rajkonkret <rajkonkret660@gmail.com>
2026-05-13 12:54:16 +0200

    [3 25] update  kalkulator.py

--- day_3/kalkulator.py
+++ day_3/kalkulator.py
@@ -3,3 +3,18 @@
 # menu z opcji
 # wyświetlić ładnie wynik -> f-string
 # obsłużyc wyjątki
+while True:
+    print("""
+    1. Dodawanie
+    2. Odejmowaie
+    3. Mnożenie
+    4. Dzielenie
+    5. Koniec
+    """)
+
+    odp = input("Podaj wybraną opcję:")  # str
+
+    # if odp == 5:
+    #     break
+    if odp not in ['1', '2', '3', '4']:
+        break
