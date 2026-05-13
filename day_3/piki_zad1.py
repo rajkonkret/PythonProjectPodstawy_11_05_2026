3a196b3584296c447e45e59daa0db4037e48c6f1
rajkonkret <rajkonkret660@gmail.com>
2026-05-13 14:39:22 +0200

    [3 37] update  pliki_zad1.py

--- day_3/piki_zad1.py
+++ day_3/piki_zad1.py
@@ -5,7 +5,7 @@
 # with - context manager w pythonie
 
 # w tworzy nowy plik, kasuje gdy taki istnieje
-with open("test.log", "w") as file:  # pod file dostaniemy filehandler
+with open("test.log", "w", encoding="utf-8") as file:  # pod file dostaniemy filehandler
     file.write("Powitanie\n")
     file.write("Jeszcze jedno\n")
     # pass
@@ -14,19 +14,24 @@ with open("test.log", "w") as file:  # pod file dostaniemy filehandler
 
 # x tworzy nowy plik
 # # gdy istnieje dostajemy bład: FileExistsError: [Errno 17] File exists: 'test.log'
-# with open("test.log", "x") as file:  # pod file dostaniemy filehandler
+# with open("test.log", "x", encoding="utf-8") as file:  # pod file dostaniemy filehandler
 #     file.write("Powitanie\n")
 #     file.write("Jeszcze jedno\n")
 #     # pass
 # # FileExistsError: [Errno 17] File exists: 'test.log'
 
 # a - dodaje na koncu istniejącego pliku
-with open("test.log", "a") as f:
+with open("test.log", "a", encoding="utf-8") as f:
     f.write("Dodane\n")
     f.write("Dodane\n")
-    f.write("Dodane\n")
+    f.write("Dośdane\n")
 
-with open('test.log', "r") as f:
+with open('test.log', "r", encoding="utf-8") as f:
     lines = f.read()
 
 print(lines)
+# Powitanie
+# Jeszcze jedno
+# Dodane
+# Dodane
+# Dośdane
