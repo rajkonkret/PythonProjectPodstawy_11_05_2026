42ca2507bb1d463ba64f537cb6b18bbd8b9da87b
rajkonkret <rajkonkret660@gmail.com>
2026-05-13 14:35:17 +0200

    [3 35] update  discount.py

---
+++ day_3/piki_zad1.py
@@ -0,0 +1,27 @@
+# działania z plikami
+# filehandler
+
+# context manager
+# with - context manager w pythonie
+
+# w tworzy nowy plik, kasuje gdy taki istnieje
+with open("test.log", "w") as file:  # pod file dostaniemy filehandler
+    file.write("Powitanie\n")
+    file.write("Jeszcze jedno\n")
+    # pass
+
+# file.write("") # ValueError: I/O operation on closed file.
+
+# x tworzy nowy plik
+# # gdy istnieje dostajemy bład: FileExistsError: [Errno 17] File exists: 'test.log'
+# with open("test.log", "x") as file:  # pod file dostaniemy filehandler
+#     file.write("Powitanie\n")
+#     file.write("Jeszcze jedno\n")
+#     # pass
+# # FileExistsError: [Errno 17] File exists: 'test.log'
+
+# a - dodaje na koncu istniejącego pliku
+with open("test.log", "a") as f:
+    f.write("Dodane\n")
+    f.write("Dodane\n")
+    f.write("Dodane\n")
