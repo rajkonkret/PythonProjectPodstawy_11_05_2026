7840333fe7e146007a087788c4bf307c6e03570f
rajkonkret <rajkonkret660@gmail.com>
2026-05-13 11:18:05 +0200

    [3 15] update  random_zad1.py

---
+++ day_3/random_zad1.py
@@ -0,0 +1,10 @@
+import random
+
+# działania na liczbach losowych
+
+"""Return random integer in range [a, b], including both end points.
+        """
+print(random.randint(1, 100))  # od 1 do 100, int
+
+print(random.randrange(1, 100))  # int, od 1 do 99
+print(random.randrange(5))  # int od 0 do 4
