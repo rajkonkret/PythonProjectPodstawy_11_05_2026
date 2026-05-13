7f6fc6d11a971279e3d6c34cecad02194ed5fd7e
rajkonkret <rajkonkret660@gmail.com>
2026-05-13 09:46:30 +0200

    [3 4] update  petle_zad1.py

--- day_3/petle_zad1.py
+++ day_3/petle_zad1.py
@@ -8,3 +8,16 @@ for i in range(20):
     pass  # nic nie rób
 
 print(i)  # 19
+
+for _ in range(15):
+    print("Test podłoga")
+    print(_)  # 14
+
+for i in range(5):
+    print(i * 2)
+    print(i + 2)
+
+print("Wyjście z pętli")
+# 8
+# 6
+# Wyjście z pętli
