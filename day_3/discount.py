d34064dc8f22218a98148d8e2e9fe586d44dc7e6
rajkonkret <rajkonkret660@gmail.com>
2026-05-13 14:00:25 +0200

    [3 31] update  discount.py

--- day_3/discount.py
+++ day_3/discount.py
@@ -31,3 +31,10 @@ object_data = datetime.now().strptime("13/05/2026", "%d/%m/%Y")
 print(object_data)  # 2026-05-13 00:00:00
 print(type(object_data))  # <class 'datetime.datetime'>
 
+# tomorrow = today + 1
+# TypeError: unsupported operand type(s) for +: 'datetime.date' and 'int'
+
+# days=0, seconds=0, microseconds=0,
+#                 milliseconds=0, minutes=0, hours=0, weeks=0
+tomorrow = today + timedelta(days=1)
+print(tomorrow)  # 2026-05-14
