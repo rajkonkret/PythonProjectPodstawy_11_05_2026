ce79bfb97843793d16fb944b488ecc991ca35097
rajkonkret <rajkonkret660@gmail.com>
2026-05-13 12:14:36 +0200

    [3 21] update  petle_zad3.py

---
+++ day_3/petle_zad3.py
@@ -0,0 +1,34 @@
+# while - pętla sterowana warunkiem
+
+# pętla nieskonczona
+# while True:
+#     print("Komunikat")
+
+licznik = 0
+while True:
+    licznik += 1  # licznik = licznik + 1
+    print("Komunikat 2 !!")
+    if licznik > 10:
+        break  # przerwanie pętli
+
+print(licznik)  # 11
+
+print(50 * "-")
+
+licznik = 0
+while licznik < 10:
+    licznik += 1
+    print("Komunikat 3 !!!")
+# --------------------------------------------------
+# Komunikat 3 !!!
+# Komunikat 3 !!!
+# Komunikat 3 !!!
+
+password = input("Podaj hasło:")
+while password != "secret":  # != - różne
+    password = input("podaj hasło:")
+# Podaj hasło:a
+# podaj hasło:asdasfd
+# podaj hasło:as
+# podaj hasło:fsddfgtrhtr
+# podaj hasło:secret
