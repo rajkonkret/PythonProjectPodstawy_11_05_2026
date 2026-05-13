ce090ec67e340d33e1edf69209a9ada31b5cd164
rajkonkret <rajkonkret660@gmail.com>
2026-05-13 09:43:39 +0200

    [3 3] update  petle_zad1.py

---
+++ day_3/petle_zad1.py
@@ -0,0 +1,10 @@
+# pętla - możliwość wykonania kodu wielokrotnie
+# for - pętla iteracyjna
+
+for i in range(5):  # od 0 do 4
+    print(i)
+
+for i in range(20):
+    pass  # nic nie rób
+
+print(i)  # 19
