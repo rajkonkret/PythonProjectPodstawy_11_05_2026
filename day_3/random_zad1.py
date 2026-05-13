ba918733599e1744836af915df6b2d1af444711e
rajkonkret <rajkonkret660@gmail.com>
2026-05-13 11:24:13 +0200

    [3 16] update  random_zad1.py

--- day_3/random_zad1.py
+++ day_3/random_zad1.py
@@ -8,3 +8,11 @@ print(random.randint(1, 100))  # od 1 do 100, int
 
 print(random.randrange(1, 100))  # int, od 1 do 99
 print(random.randrange(5))  # int od 0 do 4
+
+print(random.random())  # 0.4733209816123689 float od 0 do 0.9999999
+print(random.random() * 8)  # 7.692429343699482 float od 0 do 7.9999999
+
+lista = [67, 45, 32, 68, 90, 42, 49]
+print(lista[random.randrange(len(lista))])  # 68
+
+print(random.choice(lista))  # element z listy, 90, losuje jeden element
