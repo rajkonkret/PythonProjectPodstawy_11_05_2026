dcd95bc0775270d8dac913aa758020a848b8d6a9
rajkonkret <rajkonkret660@gmail.com>
2026-05-14 09:31:54 +0200

    [4 3] update  csv_zad2.py

--- day_3/csv_zad2.py
+++ day_3/csv_zad2.py
@@ -1,13 +1,21 @@
 import csv
 
-# filename = 'records.csv'
-filename = 'records_discount.csv'
+filename = 'records.csv'
+# filename = 'records_discount.csv'
 
 fields = []
 rows = []
 
 with open(filename, "r") as csv_f:
-    csvreader = csv.reader(csv_f, delimiter=";")
+    dialect = csv.Sniffer().sniff(csv_f.read(1024))
+    print(dialect.delimiter)  # ;
+    print(dialect.quotechar)  # "
+
+    # StopIteration - skończyły się dane
+    csv_f.seek(0)  # powrót na początek pliku
+
+    # csvreader = csv.reader(csv_f, delimiter=";")
+    csvreader = csv.reader(csv_f, delimiter=dialect.delimiter)
     print(csvreader)  # <_csv.reader object at 0x000002C8D6EA08E0>
 
     fields = next(csvreader)  # odczyta jeden wiersz, ustawi odczyt na nastepny
