f7481ca7de48cc004c9068a578dc656d4357c5f6
rajkonkret <rajkonkret660@gmail.com>
2026-05-14 12:38:55 +0200

    [4 20] update fun7.py

--- day_4/fun7.py
+++ day_4/fun7.py
@@ -13,3 +13,11 @@ connect(a=10, b=90, c=78, name="Radek")  # {'a': 10}
 def all_args(*args, **kwargs):
     print(args, kwargs)
 
+
+all_args()
+all_args(1, 2, 3, 4, 5)  # (1, 2, 3, 4, 5) {}
+all_args(a=10, b=30)  # () {'a': 10, 'b': 30}
+all_args(5, 6, 5, 6, 75, 6, name="Radek")
+# (5, 6, 5, 6, 75, 6) {'name': 'Radek'}
+
+# all_args(a=10, 1,2,3,4) # SyntaxError: positional argument follows keyword argument
