7ffbb780faf5046c7ac41b02fde14f93c7d50e59
rajkonkret <rajkonkret660@gmail.com>
2026-05-14 12:03:49 +0200

    [4 16] update fun5_kantor.py

--- day_4/fun5_kantor.py
+++ day_4/fun5_kantor.py
@@ -2,3 +2,47 @@
 # ma mieć dwie wewnętrzne funkcje: eur, usd
 # w zależności od parametru (if) zwróci jedną z funkcji (adres)
 # przekazanie kwoty do funkcji usd, eur
+
+def kantor(waluta):
+    print("Otwieram kantor")
+
+    def usd(kwota=0):
+        print(f"Wymieniam {kwota} usd na {kwota * 3.60}")
+
+    def eur(kwota=0):
+        print(f"Wymieniam {kwota} eur na {kwota * 4.2}")
+
+    if waluta == "eur":
+        return eur
+    else:
+        return usd
+
+
+kantor_usd = kantor("usd")
+kantor_eur = kantor("eur")
+# Otwieram kantor
+# Otwieram kantor
+
+kantor_usd()
+kantor_usd()
+kantor_usd()
+kantor_usd()
+# Wymieniam 0 usd na 0.0
+# Wymieniam 0 usd na 0.0
+# Wymieniam 0 usd na 0.0
+# Wymieniam 0 usd na 0.0
+
+kantor_eur(1000)
+kantor_eur(1000)
+kantor_eur(1000)
+kantor_eur()
+# Wymieniam 1000 eur na 4200.0
+# Wymieniam 1000 eur na 4200.0
+# Wymieniam 1000 eur na 4200.0
+# Wymieniam 0 eur na 0.0
+
+kwota = input("Podaj kwote:")
+kantor_eur(int(kwota))
+# Wymieniam 0 eur na 0.0
+# Podaj kwote:5678
+# Wymieniam 5678 eur na 23847.600000000002
