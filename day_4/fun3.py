376acfc0a2e64689449b32db326fe2482b028104
rajkonkret <rajkonkret660@gmail.com>
2026-05-14 11:00:17 +0200

    [4 10] update fun3

---
+++ day_4/fun3.py
@@ -0,0 +1,39 @@
+a = 10
+b = 10
+
+
+def dodaj():
+    a = 7  # zmienne lokalne, widoczne tylko wewnątrz funkcji
+    b = 8
+    print(a + b)
+
+
+def dodaj2():
+    print(a + b)  # działa na globalnych
+
+
+def dodaj3():
+    global a  # użyj zmiennej globalnej
+    a = 7  # zmieniamy wartość zmiennej globalnej
+    b = 90
+    print(a + b)
+
+
+def dodaj5():
+    a = 10
+    b = 9
+    c = a + b
+    print(c)  # c - zmienna lokalna
+
+
+print("Wartość a z góry (globalne)", a)  # Wartość a z góry (globalne) 10
+dodaj()  # 15
+print("Wartość a z góry (globalne)", a)  # Wartość a z góry (globalne) 10
+dodaj2()  # 20
+print("Wartość a z góry (globalne)", a)  # Wartość a z góry (globalne) 10
+dodaj3()  # 97
+print("Wartość a z góry (globalne)", a)  # Wartość a z góry (globalne) 7
+dodaj2()  # 7 + 10
+print("Wartość a z góry (globalne)", a)  # Wartość a z góry (globalne) 7
+dodaj5()  # 19
+# print(c)  # NameError: name 'c' is not defined
