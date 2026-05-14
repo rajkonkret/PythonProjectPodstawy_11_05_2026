496bdf751a33b9c29becc51389d4e14c1c5e72a6
rajkonkret <rajkonkret660@gmail.com>
2026-05-14 10:24:58 +0200

    [4 6] update  csv_zad3.py

--- day_4/fun1.py
+++ day_4/fun1.py
@@ -16,8 +16,33 @@ def dodaj2(a, b):  # dwa obowiązkowe argumenty a, b
     print(a + b)  # wartości lokalne
 
 
+# omninięcie problemu braku przeciązania funkcji liczbą argumentów
+def odejmij(a, b, c=0):  # argument c o wartości domyslnej
+    print(a - b - c)
+
+
 # wywołąnie funkcji
 dodaj()  # 14
 
 # dodaj2()  # TypeError: dodaj2() missing 2 required positional arguments: 'a' and 'b'
 dodaj2(5, 9)  # 14
+
+# argumenty pozycyjne
+odejmij(1, 2)  # -1
+odejmij(1, 2, 3)  # -4
+
+# argumenty po nazwie
+odejmij(b=90, a=87)
+odejmij(b=90, c=87, a=76)
+# -3
+# -101
+
+# mieszane
+odejmij(1, 2, c=89)
+dodaj2(1, b=90)
+# -90
+# 91
+
+# argumenty pozycyjne musza byc przed nazwanymi
+# odejmij(a=10, 1, 2) # SyntaxError: positional argument follows keyword argument
+
