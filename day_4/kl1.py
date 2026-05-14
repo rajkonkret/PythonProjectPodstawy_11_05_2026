4aed50456adc5f25799f5e2607a6f47be2ab9523
rajkonkret <rajkonkret660@gmail.com>
2026-05-14 15:17:05 +0200

    [4 32] update kl1.py

--- day_4/kl1.py
+++ day_4/kl1.py
@@ -14,11 +14,31 @@ class Human:
     Klasa Human opsiująca człowieka e Pythonie
     """
 
+    imie = ""
+    wiek = None
+    plec = "k"
 
-print(print.__doc__)
-print(Human.__doc__)  # Klasa Human opsiująca człowieka e Pythonie
+
+# print(print.__doc__)
+# print(Human.__doc__)  # Klasa Human opsiująca człowieka e Pythonie
 
 # pydoc - narzędzie do dokumentacji
 # cd .. - wyjscie do wyższego katalogu
 #  pydoc -b - serwer dokumentacji
 #  pydoc -w .\kl1.py - plik html z dokumentacją
+
+cz1 = Human()
+print(cz1)  # <__main__.Human object at 0x000002998F8F4EC0>
+print(cz1.plec)
+print(cz1.imie)
+print(cz1.wiek)
+# k
+#
+# None
+
+cz1.wiek = 90
+cz1.imie = "Radek"
+cz1.plec = "m"
+print(cz1.plec)  # m
+print(cz1.imie)  # Radek
+print(cz1.wiek)  # 90
