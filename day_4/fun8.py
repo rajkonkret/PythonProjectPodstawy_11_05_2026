e33eacf46da2b53839f1e043cf4a600879bfdf0f
rajkonkret <rajkonkret660@gmail.com>
2026-05-14 12:49:46 +0200

    [4 21] update fun8.py

---
+++ day_4/fun8.py
@@ -0,0 +1,21 @@
+def all_params(a, b, /, c=42, d=345):
+    print(f"{a=}, {b=}")
+    print(f"{c=}, {d=}")
+
+
+all_params(1, 2)
+# a=1, b=2
+# c=42, d=345
+all_params(1, 2, 3)
+all_params(1, 2, 3, 4)
+# a=1, b=2
+# c=3, d=4
+
+all_params(1, 2, c=8)
+all_params(1, 2, c=8, d=90)
+
+# / - oddziela parametry po nazwie od parametrów pozycyjnych
+# a, b - muszą zostać przekazane po pozycji !!!
+
+# all_params(a=1, b=2, c=3, d=4)
+# TypeError: all_params() got some positional-only arguments passed as keyword arguments: 'a, b'
