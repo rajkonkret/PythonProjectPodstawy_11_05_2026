a280ecc09952cf35c19a38525db0c82cd78b1ce8
rajkonkret <rajkonkret660@gmail.com>
2026-05-14 15:12:17 +0200

    [4 31] update fun9.py

---
+++ day_4/kl1.py
@@ -0,0 +1,24 @@
+# klasa - przepis, szablon
+# cechy (zmienne)
+# metody - funkcje w klasie
+# obiekt - instancja klasy
+# klasa musi zostac najpierw zadeklarowana
+# tworzenie obiektu uruchamia  metodę inicjalizującą (konstruktor) __init__
+# __del__ - destruktor
+# paradygmaty -> hermetyzacja, dziedziczenie, polimorfizm, abstrakcja
+
+# PascalCase , UpperCamelCase
+class Human:
+    # pass
+    """
+    Klasa Human opsiująca człowieka e Pythonie
+    """
+
+
+print(print.__doc__)
+print(Human.__doc__)  # Klasa Human opsiująca człowieka e Pythonie
+
+# pydoc - narzędzie do dokumentacji
+# cd .. - wyjscie do wyższego katalogu
+#  pydoc -b - serwer dokumentacji
+#  pydoc -w .\kl1.py - plik html z dokumentacją
