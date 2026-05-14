36e3163a65f2eee584d306895b492ecf6e2c4f97
rajkonkret <rajkonkret660@gmail.com>
2026-05-14 09:23:55 +0200

    [4 2] update  csv_zad2.py

--- day_3/csv_zad2.py
+++ day_3/csv_zad2.py
@@ -1,12 +1,13 @@
 import csv
 
-filename = 'records.csv'
+# filename = 'records.csv'
+filename = 'records_discount.csv'
 
 fields = []
 rows = []
 
 with open(filename, "r") as csv_f:
-    csvreader = csv.reader(csv_f)
+    csvreader = csv.reader(csv_f, delimiter=";")
     print(csvreader)  # <_csv.reader object at 0x000002C8D6EA08E0>
 
     fields = next(csvreader)  # odczyta jeden wiersz, ustawi odczyt na nastepny
@@ -18,3 +19,5 @@ print("Fields:", fields)
 print("Rows:", rows)
 # Fields: ['name', 'branch', 'year', 'cgpa']
 # Rows: [['radek', 'coe', '3', '0']]
+# ['6;today;200']]
+#  ['6', 'today', '200']
