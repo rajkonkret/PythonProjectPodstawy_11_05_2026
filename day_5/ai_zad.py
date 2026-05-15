c1884560b0c8bfd6b9997d22f4dbbb975d28395c
rajkonkret <rajkonkret660@gmail.com>
2026-05-15 16:00:11 +0200

    [5 43] update wykres3_3d.py

---
+++ day_5/ai_zad.py
@@ -0,0 +1,24 @@
+print("")
+print("")
+print("")
+print("")
+# napisz funkcje srednia ocen
+print("")
+def srednia():
+    print("")
+    print("")
+    print("")
+
+
+
+def srednia(oceny):
+    print("")
+    print("")
+
+print("")
+oceny = [4, 5, 3, 4, 5]
+def srednia(oceny):
+    print("")
+    print("")
+    print("")
+    print("")
