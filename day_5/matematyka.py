925593c6ef843f74c13d3c67951d9451b1b53813
rajkonkret <rajkonkret660@gmail.com>
2026-05-15 11:41:31 +0200

    [5 7] update matematyka.py

---
+++ day_5/matematyka.py
@@ -0,0 +1,11 @@
+import math
+
+print(math.pi)  # 3.141592653589793
+print(len(str(math.pi)))  # 17
+
+print(math.sqrt(25))  # 5.0
+# SciPy, nympy, pandas
+
+# sin 30 stopni
+angle_radians = math.radians(30)
+print(math.sin(angle_radians))  # 0.49999999999999994
