ba3dd89d8ef910b1e9f8885b6db62672bdbd7804
rajkonkret <rajkonkret660@gmail.com>
2026-05-15 15:24:49 +0200

    [5 38] update wykres2.py

--- day_5/wykres2.py
+++ day_5/wykres2.py
@@ -4,6 +4,8 @@ labels = ["Jabłka", "Banany", "Winogrono", "Pomarańcza"]
 sizes = [30, 25, 20, 45]
 colors = ['red', 'blue', 'green', 'yellow']
 
+plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%')
+
 plt.title("Wykres kołowy")
 
 plt.axis('equal')
