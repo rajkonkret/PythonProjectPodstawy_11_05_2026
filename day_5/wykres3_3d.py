857b7eb63de5655c3b9adfaaf30a008ebed695cc
rajkonkret <rajkonkret660@gmail.com>
2026-05-15 15:38:42 +0200

    [5 42] update wykres3_3d.py

--- day_5/wykres3_3d.py
+++ day_5/wykres3_3d.py
@@ -3,3 +3,10 @@ import matplotlib.pyplot as plt
 xs = [0, 1, 2, 3, 4, 5]
 ys = [0, 2, 4, 2, 0, -2]
 zs = [0, 1, 0, 1, 0, 1]
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.plot_trisurf(xs, ys, zs, color='lightblue', edgecolor='gray')
+
+plt.show()
