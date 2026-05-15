9fba7fb06ea1773696dd3868181646def324b449
rajkonkret <rajkonkret660@gmail.com>
2026-05-15 14:38:53 +0200

    [5 31] update okno1.py

---
+++ day_5/okno1.py
@@ -0,0 +1,26 @@
+# tkinter, pyqt, pyside, gtk+
+import tkinter
+
+
+class MyGui:
+    """
+    Okienko
+    """
+
+    def __init__(self):
+        self.main_window = tkinter.Tk()
+
+        self.label1 = tkinter.Label(self.main_window, text="Witaj Świecie")
+        self.label2 = tkinter.Label(self.main_window, text="Python")
+        self.label3 = tkinter.Label(self.main_window, text="Top")
+        self.label4 = tkinter.Label(self.main_window, text="Bottom")
+
+        self.label1.pack(side="left")
+        self.label2.pack(side="right")
+        self.label3.pack(side="top")
+        self.label4.pack(side="bottom")
+
+        tkinter.mainloop()
+
+
+my_gui = MyGui()
