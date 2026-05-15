5fde004c5c4cf971409241f221d016c6db8bea15
rajkonkret <rajkonkret660@gmail.com>
2026-05-15 11:44:48 +0200

    [5 8] update matematyka.py

--- day_5/matematyka.py
+++ day_5/matematyka.py
@@ -9,3 +9,8 @@ print(math.sqrt(25))  # 5.0
 # sin 30 stopni
 angle_radians = math.radians(30)
 print(math.sin(angle_radians))  # 0.49999999999999994
+
+angles = [0, 30, 45, 60, 90]
+sin_values = [math.sin(math.radians(a)) for a in angles]
+print(sin_values)
+# [0.0, 0.49999999999999994, 0.7071067811865476, 0.8660254037844386, 1.0]
