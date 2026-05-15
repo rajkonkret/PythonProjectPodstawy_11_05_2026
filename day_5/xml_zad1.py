154b50d7b900ab734e400d53ec159225b2c56594
rajkonkret <rajkonkret660@gmail.com>
2026-05-15 13:45:37 +0200

    [5 23] update xml_zad1.py

---
+++ day_5/xml_zad1.py
@@ -0,0 +1,20 @@
+# xml
+# język tagów
+
+from xml.dom import minidom
+
+root = minidom.Document()  # <?xml version="1.0" ?>
+
+xml = root.createElement('root')
+root.appendChild(xml)  # <root> </root>
+
+productChild = root.createElement('product')  # <product>
+productChild.setAttribute("name", "RADEK")  # name="RADEK
+xml.appendChild(productChild)
+
+xml_str = root.toprettyxml(indent="\t")
+print(xml_str)
+# <?xml version="1.0" ?>
+# <root>
+# 	<product name="RADEK"/>
+# </root>
