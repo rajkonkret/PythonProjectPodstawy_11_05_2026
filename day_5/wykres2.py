f73afee91b21ac545f166a5ed0fd8c8484783f73
rajkonkret <rajkonkret660@gmail.com>
2026-05-15 15:21:59 +0200

    [5 37] update wykres2.py

--- day_5/wykres2.py
+++ day_5/wykres2.py
@@ -3,3 +3,8 @@ import matplotlib.pyplot as plt
 labels = ["Jabłka", "Banany", "Winogrono", "Pomarańcza"]
 sizes = [30, 25, 20, 45]
 colors = ['red', 'blue', 'green', 'yellow']
+
+plt.title("Wykres kołowy")
+
+plt.axis('equal')
+plt.show()
