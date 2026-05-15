e6fb05a573b04d0c74f8854d8349403ba262e58b
rajkonkret <rajkonkret660@gmail.com>
2026-05-15 12:58:39 +0200

    [5 19] update baza1.py

---
+++ day_5/baza2.py
@@ -0,0 +1,34 @@
+# baza danych - system przechowywania danych
+# silnik - mechanizm przechowywania, zarządzzania i dostępu do danych
+# bazy relacyjne, nierelacyjne
+# sql, nosql
+# ms sqlserver, oracle, mysql, mariadb, postgress, terradata, sqlite
+
+import sqlite3
+
+try:
+    conn = sqlite3.connect("baza_danych.db")
+    c = conn.cursor()
+    print("Baza danych została podłączona")
+
+    query  = """
+    CREATE TABLE IF NOT EXISTS developers (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    salary REAL NOT NULL);
+            """
+
+    c.execute(query)
+    conn.commit()
+
+except sqlite3.Error as e:
+    print("Bład podłaczenia bazy danych:", e)
+finally:
+    if conn:
+        conn.close()
+        print("Podłaczenie zostało zamknięte")
+# Baza danych została podłączona
+# Podłaczenie zostało zamknięte
+
+# powerquery, powerbi, pgadmin, dbeaver, TablePlus, sqldeveloper
